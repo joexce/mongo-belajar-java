f3ebda15c599530dd99e697a485b5d0349266846
jonathan <jonathan@sayurbox.com>
2020-01-15 11:21:03 +0700

    remove kafka

--- RestMongoDb/PersonalApi/src/main/java/Home/MainApp.java
+++ RestMongoDb/PersonalApi/src/main/java/Home/MainApp.java
@@ -4,17 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import org.springframework.kafka.annotation.KafkaListener;
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args){
             SpringApplication.run(MainApp.class, args);
         }
-
-
-    @KafkaListener(id = "kafka", topics = "pesan-saya")
-    public void dltListen(String in) {
-        System.out.println("Data yang ditarik : " + in);
-    }
 }
